8f08104042c267d3cd7738489784689e999ab99d
Keertan Kini <krkini16@mit.edu>
2014-04-13 18:09:53 -0400

    JQuery added back in

--- KVfiles/kv.js
+++ KVfiles/kv.js
@@ -1,3 +1,17 @@
 $(function() {
-    // $( "#tabs" ).tabs();
+
+    activaTab('nameList');
+    //  $('.tabs').bind('change', function (e) {
+    //     // e.target is the new active tab according to docs
+    //     // so save the reference in case it's needed later on
+    //     window.activeTab = e.target;
+    //     // display the alert
+    //     alert("hello");
+    //     // Load data etc
+    // });
   });
+
+function activaTab(tab){
+	console.log(tab);
+    $('.nav-tabs a[href="#' + tab + '"]').tab('show');
+};
