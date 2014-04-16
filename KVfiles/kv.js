a7159934c756ddfc662f4f80a0d1a3a7fd29c10a
Keertan Kini <krkini16@mit.edu>
2014-04-15 22:07:48 -0400

    Tried to add Jeffrey's calendar

--- KVfiles/kv.js
+++ KVfiles/kv.js
@@ -1,15 +1,5 @@
 $(function() {
 
-    // activaTab('freedomList');
-    //  $('.tabs').bind('change', function (e) {
-    //     // e.target is the new active tab according to docs
-    //     // so save the reference in case it's needed later on
-    //     window.activeTab = e.target;
-    //     // display the alert
-    //     alert("hello");
-    //     // Load data etc
-    // });
-
 	$('#actualTabs a[href="#nameList"]').click(function (e) {
 		  e.preventDefault();
 		  $(this).tab('show');
